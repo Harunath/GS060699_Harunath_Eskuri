1819deff59981035bf6c3c9979951c9e22cbbcb5
harunath <harunath04@gmail.com>
2025-03-11 19:55:53 +0530

    api url bug - extra "/"

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,2 +1,2 @@
 // export const api = "http://localhost:5000/api/"
-export const api = "https://gs060699-harunath-eskuri-backend.onrender.com/api/";
+export const api = "https://gs060699-harunath-eskuri-backend.onrender.com/api";
